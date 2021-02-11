4e60783a3000c6bb0b6627535ac5db1b3d4ae1f1
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-02-11 15:12:11 +0000

    wyswietlanie opisu

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
-let button = document.querySelector('input');
-button.textContent = "Klik działa!";
-button.onclick = function() {
-    alert('Klik działa!');
+let btnDisplay = document.querySelector('button');
+
+
+btnDisplay.onclick = function () {
+    let pDescription = document.getElementById("opis");
+    pDescription.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
 }
+
+
