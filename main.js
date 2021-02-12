ead1488e532235463bb462fcb076c68442f6aec5
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-02-12 13:43:23 +0000

    sprawdzanie liczby2 update

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 
 
 
-let x = 12;
-let y = 20;
-console.log(y);
+let any = 1;
+let y = any;
+let x = any;
+
+function sprawdzanieLiczby(x,y) {
+    if ((x === 20)||(y === 20)) {
+        console.log('true');
+    }
+    else if ( x+y <= 20) {
+        console.log('true');
+    }
+    else { console.log('false');}
+}
+
+sprawdzanieLiczby(67, 12);
+console.log(sprawdzanieLiczby(67, 12));
