c26cc67ea5b8bb93cabf508ffa45a0871558f00e
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-02-12 11:18:18 +0000

    tablica 5 liczb

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+ let num1 = 5;
+ let num2 = 10;
+ let num3 = 15;
+ let num4 = 20;
+ let num5 = 25;
+ let tabela = [num1, num2, num3, num4, num5];
+
+//  console.log(tabela);
+
+function pierwszyElement(tabela) {
+    let result = num1;  
+    return result; 
+}
+
+pierwszyElement(tabela);
+console.log(num1);
