cac05edbfbb075a2783b969695039f036be96e2a
*bibisk* <*beata.g.sukiennik@gmail.com*>
2021-02-12 13:36:28 +0000

    sprawdzanie liczby 2

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 
 
 
-
-
- let num1 = 5;
- let num2 = 10;
- let num3 = 15;
- let num4 = 20;
- let num5 = 25;
- let tabela = [num1, num2, num3, num4, num5];
-
-//  console.log(tabela);
-
-function pierwszyElement(tabela) {
-    let result = num1;  
-    return result; 
-}
-
-pierwszyElement(tabela);
-console.log(num1);
+let x = 12;
+let y = 20;
+console.log(y);
