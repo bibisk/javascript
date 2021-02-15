73c1b854afa2a67f87b85fe7aaf6516ca8d85b0e
bibisk <78370441+bibisk@users.noreply.github.com>
2021-02-15 12:39:32 +0000

    Długość stringa poprawka

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 
-let btnDisplay = document.getElementById("add");
-// console.log(buttonDisplay);
+// let btnDisplay = document.getElementById("add");
+// // console.log(buttonDisplay);
 
-btnDisplay.onclick = function() {
-    let pDescription = document.getElementById("tekst");
-    pDescription.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus officia quaerat soluta explicabo tempora nesciunt, dignissimos aperiam. Nihil officiis amet debitis! Illo corrupti recusandae nemo, dolores nulla hic accusantium!';
-}
+// btnDisplay.onclick = function() {
+//     let pDescription = document.getElementById("tekst");
+//     pDescription.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus officia quaerat soluta explicabo tempora nesciunt, dignissimos aperiam. Nihil officiis amet debitis! Illo corrupti recusandae nemo, dolores nulla hic accusantium!';
+// }
 
-let btnHide = document.getElementById("remove");
-// console.log(btnHide);
+// let btnHide = document.getElementById("remove");
+// // console.log(btnHide);
 
-btnHide.onclick = function() {
-    let pDescriptionNone = document.getElementById("tekst");
-    pDescriptionNone = '';
-}
+// btnHide.onclick = function() {
+//     let pDescriptionNone = document.getElementById("tekst");
+//     pDescriptionNone = '';
+// }
 
 
 
+let tekst1 = 'Uwielbiam Java Script';
+let tekst2 = 'Jestem świetnym programistą';
+
+function compare (tekst1, tekst2) {
+    if (tekst1.lenght > tekst2.lenght){
+        console.log(tekst1); }
+    else if (tekst2.lenght > tekst1.lenght) {
+        console.log(tekst2); }
+    }
+
+
+compare ('Uwielbiam Java Script', 'Jestem świetnym programistą');
+console.log(tekst1, tekst2); 
