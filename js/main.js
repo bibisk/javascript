d18c9b743c69212b81479d1e4ac16e655770976a
bibisk <78370441+bibisk@users.noreply.github.com>
2021-02-15 10:09:07 +0000

    działania matematyczne - js36

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+
+function maths(num1, num2) {
+    let addingResult = num1 + num2;
+    if (addingResult >= 0) {
+        console.log('Wynik dodawania wynosi ' + addingResult);
+    } else {
+        console.log('Wynik jest nieprawidłowy');
+    }
+    let subtractResult = num1 - num2;
+    if (subtractResult >= 0) {
+        console.log('Wynik odejmowania wynosi ' + substractResult);
+    } else {
+        console.log('Wynik jest nieprawidłowy');
+    }
+    let multiplyResult = num1 * num2; {
+    if (multiplyResult >=0) {
+        console.log('Wynik mnożenia wynosi ' + multiplyResult);
+    } else {
+        console.log('Wynik jest nieprawidłowy');
+    }
+    } 
+    
+}
+
+
+maths(4, 7);
+maths(5, 108);
+maths(15 - 78);
+maths(43 * 15);
+
+
+
+
